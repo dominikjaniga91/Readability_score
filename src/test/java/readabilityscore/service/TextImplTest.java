19da13ea85b06e6e2e59a45fb26ef726ce8ca678
dominikjaniga91 <dominikjaniga91@gmail.com>
2020-07-12 20:05:16 +0200

    Add ParametrizedTest and csv file to counter test

--- src/test/java/readabilityscore/service/TextImplTest.java
+++ src/test/java/readabilityscore/service/TextImplTest.java
@@ -3,7 +3,8 @@ package readabilityscore.service;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
-import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvFileSource;
 import readabilityscore.service.implementation.TextImpl;
 
 public class TextImplTest {
@@ -15,11 +16,10 @@ public class TextImplTest {
         textImpl = new TextImpl();
     }
 
-    @Test
-    @DisplayName("Should return appropriate number of words in text")
-    void shouldReturnNumberOfWords(){
-        String text = "This is the front page of the Simple English Wikipedia. Wikipedias are places where people work together to write encyclopedias in different languages.";
-        int expected = 23;
+    @ParameterizedTest(name = "{0} should give result: {1}")
+    @CsvFileSource(resources = "/data.csv", delimiter = ';')
+    @DisplayName("Should return appropriate number of words in text:")
+    void shouldReturnNumberOfWords(String text, int expected){
         int  actual = textImpl.counter(text, " ");
         Assertions.assertEquals(expected, actual);
     }
