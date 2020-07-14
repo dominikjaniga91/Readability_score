102873879798c8ea2909ca59e2634370855d7b5e
dominikjaniga91 <dominikjaniga91@gmail.com>
2020-07-14 14:31:11 +0200

    Create ScoreCalculation with calculateARI method

---
+++ src/main/java/readabilityscore/service/ScoreCalculation.java
@@ -0,0 +1,4 @@
+package readabilityscore.service;
+
+public interface ScoreCalculation {
+}
---
+++ src/main/java/readabilityscore/service/implementation/ScoreCalculationImpl.java
@@ -0,0 +1,25 @@
+package readabilityscore.service.implementation;
+
+import readabilityscore.service.FileReader;
+import readabilityscore.service.ScoreCalculation;
+
+public class ScoreCalculationImpl implements ScoreCalculation {
+
+    private final TextImpl textImpl     = new TextImpl();
+    private final FileReader reader     = new FileReader();
+    private final String text           = reader.readTextFromFile("src/main/resources/input.txt");
+
+    // constants required to calculations
+    private final long characters       = textImpl.getNumberOfCharacters(text);
+    private final long sentences        = textImpl.counter(text, "[!//.//?]");
+    private final long words            = textImpl.counter(text, " ");
+    private final long syllables        = textImpl.getNumberOfSyllables(text);
+    private final long polysyllables    = textImpl.getNumberOfPolysyllables(text);
+    private final double L              = (double) characters / words * 100;
+    private final double S              = (double) sentences / words * 100;
+
+
+    private double calculateARI(){
+        return 4.71 * characters / words + 0.5 * words / sentences - 21.43;
+    }
+}
