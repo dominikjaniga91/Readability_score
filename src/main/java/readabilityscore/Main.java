96c664223a2c1170446b534ed72f411ee780dbbb
dominikjaniga91 <dominikjaniga91@gmail.com>
2020-07-14 17:37:06 +0200

    Create class instances in main method

--- src/main/java/readabilityscore/Main.java
+++ src/main/java/readabilityscore/Main.java
@@ -1,7 +1,17 @@
 package readabilityscore;
 
+import readabilityscore.service.FileReader;
+import readabilityscore.service.TextData;
+import readabilityscore.service.implementation.ScoreCalculationImpl;
+import readabilityscore.service.implementation.TextImpl;
+
 public class Main {
     public static void main(String[] args) {
 
+        var text        = new FileReader().readTextFromFile("src/main/resources/input.txt");
+        var data        = new TextData(new TextImpl(), text);
+        var result      = new ScoreCalculationImpl().calculateReadabilityScore(data);
+
+        System.out.println(result);
     }
 }
